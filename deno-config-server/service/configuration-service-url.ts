d44f15a2ab0a9c5b8c0807ba2e21e389b09ed650
Peter Filina <peter.filina@hotmail.com>
2020-05-22 13:17:48 +0200

    better try catch

--- deno-config-server/service/configuration-service-url.ts
+++ deno-config-server/service/configuration-service-url.ts
@@ -4,7 +4,7 @@ export async function resolve(
   url: string,
   name: String
 ): Promise<ConfigurationItem> {
-    return new Promise<ConfigurationItem>( async (resolve, reject) => {
+    return new Promise<ConfigurationItem>(async (resolve, reject) => {
         console.log("Looking for configuration item: %s", name);
         console.log("Fetching configuration file from URL: %s", url);
         if (
@@ -12,22 +12,22 @@ export async function resolve(
             /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
           ) === null
         ) {
-          console.error("Invalid URL: %s", url);
           reject (new Error("Invalid URL"));
         }
       
+        let items : ConfigurationItem[] | undefined;
         try {
           const content = await fetch(url);
-          let items : ConfigurationItem[] = await content.json();
+          items = await content.json();
           console.log(items);
-
-          let found = items.find(e => {return e.name == name});
-          console.log("found: %o", found)
-
-          resolve(found);
         } catch (e) {
           reject(e);
+          return;
         }
+
+        const found = items?.find(e => {return e.name == name});
+        console.log("found: %o", found)
+        resolve(found);
       });
 }
 
